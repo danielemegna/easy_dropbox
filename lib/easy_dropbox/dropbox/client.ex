9c7e6146f20f48c8058c72cd209aff3a7e8d1a2c
Daniele Megna <daniele.megna@casavo.com>
2021-05-11 12:01:46 +0200

    Refactoring in Dropbox.Client

--- lib/easy_dropbox/dropbox/client.ex
+++ lib/easy_dropbox/dropbox/client.ex
@@ -6,41 +6,44 @@ defmodule EasyDropbox.Dropbox.Client do
   end
 
   defp fetch_ebooks_with(token) do
-    url = "https://api.dropboxapi.com/2/files/list_folder"
-    body = %{
-      "path" => "/ebooks",
-      "recursive" => true,
-      "include_media_info" => false,
-      "include_deleted" => false,
-      "include_has_explicit_shared_members" => false,
-      "include_mounted_folders" => false,
-      "include_non_downloadable_files" => false,
-    }
-    headers = [
-      {"Authorization", "Bearer #{token}"},
-      {"Content-Type", "application/json"},
-    ]
-
-    response = HTTPoison.post!(url, Jason.encode!(body), headers)
-
-    response.body
+    HTTPoison.post!(
+      "https://api.dropboxapi.com/2/files/list_folder",
+      Jason.encode!(%{
+        "path" => "/ebooks",
+        "recursive" => true,
+        "include_media_info" => false,
+        "include_deleted" => false,
+        "include_has_explicit_shared_members" => false,
+        "include_mounted_folders" => false,
+        "include_non_downloadable_files" => false,
+      }),
+      [
+        {"Authorization", "Bearer #{token}"},
+        {"Content-Type", "application/json"},
+      ]
+    )
+    |> Map.get(:body)
     |> Jason.decode!()
     |> Map.get("entries")
     |> Enum.map(&(Map.take(&1, ["id", "name", "path_display"])))
   end
 
   defp refresh_token do
-    url = "https://api.dropbox.com/oauth2/token"
-    body = {:multipart, [
-      {"grant_type", "refresh_token"},
-      {"refresh_token", System.get_env("DROPBOX_REFRESHABLE_TOKEN")}
-    ]}
-    credentials = System.get_env("DROPBOX_APP_KEY") <> ":" <> System.get_env("DROPBOX_APP_SECRET")
-    headers = [{"Authorization", "Basic #{Base.encode64(credentials)}"}]
+    basic_auth_credentials = System.get_env("DROPBOX_APP_KEY") <> ":" <> System.get_env("DROPBOX_APP_SECRET")
 
-    response = HTTPoison.post!(url, body, headers)
-    
-    response.body
+    HTTPoison.post!(
+      "https://api.dropbox.com/oauth2/token",
+      {
+        :multipart, [
+          {"grant_type", "refresh_token"},
+          {"refresh_token", System.get_env("DROPBOX_REFRESHABLE_TOKEN")}
+        ]
+      },
+      [
+        {"Authorization", "Basic #{Base.encode64(basic_auth_credentials)}"}
+      ]
+    )
+    |> Map.get(:body)
     |> Jason.decode!()
     |> Map.get("access_token")
   end
