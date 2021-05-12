f17e0a913b81a62c548c897bae8c7b1d9bac1a0b
Daniele Megna <daniele.megna@casavo.com>
2021-05-12 10:44:25 +0200

    First idea of "download" ... to validate
    maybe to download the file and return as response is not the most
    performing options... we should let the page do the POST for us

--- test/easy_dropbox/dropbox/client_test.exs
+++ test/easy_dropbox/dropbox/client_test.exs
@@ -8,4 +8,11 @@ defmodule EasyDropbox.Dropbox.ClientTest do
     assert Enum.count(ebooks) > 0
   end
 
+  test "download ebooks with an id" do
+    assert %{byte_content: byte_content, headers: headers} = Client.download_ebook("id:_Tm5Nio06KMAAAAAAAAChw")
+    assert Enum.count(headers) == 2
+    assert is_binary(byte_content)
+    assert byte_size(byte_content) > 0
+  end
+
 end
